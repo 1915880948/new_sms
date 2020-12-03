682652df7d35f43f14b3149f23fd049aebb35f0d
daisy <1915880948@qq.com>
2020-12-03 16:57:25 +0800

    优化---redis

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -3,6 +3,8 @@
 namespace app\index\controller;
 
 use app\common\controller\Frontend;
+use Predis\Client;
+use think\Env;
 
 class Index extends Frontend
 {
@@ -13,10 +15,17 @@ class Index extends Frontend
 
     public function index()
     {
-//        $redis = new \Redis();
-//        $redis->connect('39.104.20.84', '6379');
+        $redis = new \Redis();
+        $redis->connect('39.104.20.84', '6379');
 //$auth = $redis->auth('');
-//        print_r("Server is running: " . $redis->ping());
+
+//        $redis = new Client([
+//            'host' => Env::get('redis3.host'),
+//            'port' => Env::get('redis3.port'),
+//            'password' => Env::get('redis3.password')
+//        ]);
+        print_r("Server is running: " . $redis->ping());
+        print_r("Server is running: " . $redis->get('test-name'));
         phpinfo();
         exit;
         //$this->redirect('/admin.php/index/login');
