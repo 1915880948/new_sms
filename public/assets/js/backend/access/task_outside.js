436510e25d6b88877eade00d31ec8de7e952ffc3
huyonggang <huyonggang@shumingdata.com>
2021-01-29 14:41:27 +0800

    功能优化

--- public/assets/js/backend/access/task_outside.js
+++ public/assets/js/backend/access/task_outside.js
@@ -26,19 +26,19 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                 columns: [
                     [
                         {checkbox: true},
-                        {field: 'task_id', title: __('Task_id')},
+                        {field: 'task_id', title: __('Task_id'),operate:false},
                         {field: 'channel', title: __('Channel')},
                         {field: 'product', title: __('Product')},
-                        {field: 'startdate', title: __('Startdate'), operate:'RANGE', addclass:'datetimerange'},
-                        {field: 'enddate', title: __('Enddate'), operate:'RANGE', addclass:'datetimerange'},
+                        {field: 'total_number', title: __('Total_number'),operate:false},
+                        {field: 'number', title: __('Number'),operate:false},
+                        {field: 'startdate', title: __('Startdate'), operate:false},
+                        {field: 'enddate', title: __('Enddate'), operate:false},
                         // {field: 'type', title: __('Type'),},
-                        {field: 'is_idfa', title: __('Is_idfa'),
+                        {field: 'is_idfa', title: __('Is_idfa'),operate:false,visible:false,
                             formatter: Table.api.formatter.normal,
                             searchList:{'0':'imei', '1':'idfa'}
                         },
-                        {field: 'total_number', title: __('Total_number'),operate:false},
-                        {field: 'number', title: __('Number'),operate:false},
-                        {field: 'source_no', title: __('Source_no')},
+                        //{field: 'source_no', title: __('Source_no')},
                         {field: 'status', title: __('Status'),
                             formatter: Table.api.formatter.normal,
                             searchList:{1:'提交',2:'处理中', 3:'IMEI查询中', 4:'处理完毕'}
